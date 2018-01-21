2f773acc1dacdde8d9fb6e5316f61c981501273f
unknown <lwyang37@hotmail.com>
2018-01-21 14:20:30 -0500

    Added a chart component by using react-sparklines and use the chart to display the city's temperature, pressure and humidity

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Chart from '../components/chart';
+
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
-
+        const temps = cityData.list.map(weather => weather.main.temp);
+        //console.log(temps);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+        
+        
         return (
             <tr key={name}>
                 <td>{name}</td>
+                <td><Chart data={temps} color="orange" /></td>
+                <td><Chart data={pressures} color="green" /></td>
+                <td><Chart data={humidities} color="blue" /></td>
             </tr>
         );
     }
@@ -19,7 +29,7 @@ class WeatherList extends Component {
                     <tr>
                         <th>City</th>
                         <th>Temperature</th>
-                        <th>Presure</th>
+                        <th>Pressure</th>
                         <th>Humidity</th>
                     </tr>
                 </thead>
